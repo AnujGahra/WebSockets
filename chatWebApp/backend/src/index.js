ad4f3a4ce69ef9930fb23c0b654b5b18919b14e9
AnujGahra <anujgahra244304@gmail.com>
2025-03-06 18:55:31 +0530

    MongoDB connected

--- chatWebApp/backend/src/index.js
+++ chatWebApp/backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -19,5 +20,6 @@ app.use(cors({
 
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on PORT: ${PORT}`)
 })
