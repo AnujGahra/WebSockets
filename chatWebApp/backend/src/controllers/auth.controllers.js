4c428a87199e604f0e773f21003f24758a22ffb6
AnujGahra <anujgahra244304@gmail.com>
2025-03-08 00:14:57 +0530

    add authControllers

--- chatWebApp/backend/src/controllers/auth.controllers.js
+++ chatWebApp/backend/src/controllers/auth.controllers.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { generateToken } from "../utils/utils.js";
 
+// Bussiness Logic for SignUp User
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
@@ -43,17 +44,65 @@ export const signup = async (req, res) => {
         email: newUser.email,
         profilePic: newUser.profilePic,
       });
-    }else{
-        res.status(400).json({message: "Invalid user data"})
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
     }
   } catch (error) {
     console.log("Error in signup controller:", error.message);
-    res.status(500).json({message: "Internal server error"});
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
-export const login = async (req, res) => {};
+// Bussiness Logic for Login User
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
 
-export const logout = async (req, res) => {};
+    const user = await User.findOne({ email });
 
-export const updateProfile = async (req, res) => {};
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Bussiness Logic for Logout
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Bussiness Logic for UpdateProfile 3:46:35
+export const updateProfile = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
